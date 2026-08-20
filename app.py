af182d7dcb73666c830bccbe300e44389c1239da
Alessandro <alessandrocouto.ti@gmail.com>
2026-08-20 17:28:57 -0400

    Corriginfo erro CORS

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import sqlite3
 
 app = Flask(__name__)
+CORS(app)
+
 
 def conectar():
     conexao = sqlite3.connect("contatos.db")
